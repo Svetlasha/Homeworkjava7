3b453e467bfca109d1cc7f2a06a1ea61982074ae
Svetlana Gurenko <svetlashaaaa@gmail.com>
2023-03-17 20:43:40 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,61 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int monthMaximumSales(long[] sales) {
+        int monthMaximum = 0;
+        long maximumSale = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= maximumSale) {
+                monthMaximum = i;
+                maximumSale = sales[i];
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+
+    public int monthsBelowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int monthsAboveAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+}
+
